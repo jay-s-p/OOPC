36994f622a1036e52a63aaaee6b40235dd2b5754
jay <jaysparmar303@gmail.com>
2022-02-07 12:44:23 +0530

    fasdfadf

---
+++ primitiveToClass.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Zzz
+{
+    int a;
+public:
+    Zzz(){}
+    Zzz(int x)
+    {
+        a = x;
+    }
+    void getData(int x)
+    {
+        a = x;
+    }
+    void putData()
+    {
+        cout << a;
+    }
+    // Zzz operator=(int x)
+    // {
+    //     a = x;
+    // }
+};
+main()
+{
+    Zzz d;
+    int x = 5;
+    d = x;
+    d.putData();
+}
