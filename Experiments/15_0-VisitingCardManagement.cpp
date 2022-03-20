7ec7124a0810f79155645101aab751d7fa479700
jay <jaysparmar303@gmail.com>
2022-03-20 22:58:02 +0530

    update Exp 15

--- Experiments/15_0-VisitingCardManagement.cpp
+++ Experiments/15_0-VisitingCardManagement.cpp
@@ -7,26 +7,44 @@
 using namespace std;
 class VCard
 {
-    string _cName, _email, _address, _contect;
+    string
+        *__cName,
+        *__email,
+        *__address,
+        *__contact;
 
 public:
+    VCard()
+    {
+        __cName = new string;
+        __email = new string;
+        __address = new string;
+        __contact = new string;
+    }
+    ~VCard()
+    {
+        delete __cName;
+        delete __email;
+        delete __address;
+        delete __contact;
+    }
     void getData()
     {
-        cout << "\nEnter Company name, address, Email, and Contect : ";
-        cin >> _cName >> _address >> _email >> _contect;
+        cout << "\nEnter Company name, address, Email, and Contact : ";
+        cin >> *__cName >> *__address >> *__email >> *__contact;
     }
     void printDetails()
     {
         cout << endl;
-        cout << "Company name   : " << _cName << endl
-             << "Address        : " << _address << endl
-             << "Email          : " << _email << endl
-             << "Contect        : " << _contect << endl;
+        cout << "Company name   : " << *__cName << endl
+             << "Address        : " << *__address << endl
+             << "Email          : " << *__email << endl
+             << "Contect        : " << *__contact << endl;
     }
     // --> For Case Sensitive
     bool match(const string &cName)
     {
-        if (cName == _cName)
+        if (cName == *__cName)
             return true;
         else
             return false;
@@ -34,19 +52,29 @@ public:
     // --> For Searching Case Insensitive
     bool imatch(const string &cName)
     {
-        if (cName.size() != _cName.size())
+        if (cName.size() != (*__cName).size())
             return false;
-        for (int i = 0; i < _cName.size(); ++i)
-            if (tolower(cName[i]) != tolower(_cName[i]))
+        for (int i = 0; i < (*__cName).size(); ++i)
+            if (tolower(cName[i]) != tolower((*__cName)[i]))
                 return false;
         return true;
     }
 };
+void printLine()
+{
+    cout << "---------------------------------------";
+}
+void printMsg(string s)
+{
+    system("cls");
+    cout << s;
+}
 main()
 {
-    VCard vCards[999];
+    VCard vCards[20];
     int size;
     string choice, temp;
+    char flag;
 
     cout << "Enter no of Visiting Cards : ";
     cin >> size;
@@ -73,39 +101,69 @@ main()
 
         else if (choice == "print" || choice == "1")
         {
-            cout << "-----------------------------------------------------";
+            printLine();
             for (int i = 0; i < size; i++)
                 vCards[i].printDetails();
-            cout << "-----------------------------------------------------";
+            printLine();
         }
 
         else if (choice == "add" || choice == "2")
         {
-            size = size + 1;
+            ++size;
             vCards[size - 1].getData();
+            printMsg("*** Inserted Succesfully ***");
         }
 
         else if (choice == "delete" || choice == "3")
         {
-            cout << "\n\nComing Soon...";
-        }
-
-        else if (choice == "update" || choice == "4")
-        {
-            cout << "Enter Company name for update : ";
+            cout << "Enter Company name for DELETE : ";
             cin >> temp;
             for (int i = 0; i < size; i++)
             {
                 if (vCards[i].imatch(temp))
                 {
                     system("cls");
-                    cout << "OLD Details :-";
+                    printLine();
                     vCards[i].printDetails();
-                    cout << "\nEnter new Details :-";
+                    printLine();
+                    cout << "\nConfirm Delete this info (y/n) : ";
+                    cin >> flag;
+
+                    if (flag == 'y')
+                    {
+                        for (int j = i; j < size - 1; j++)
+                            vCards[j] = vCards[j + 1];
+                        --size;
+                        printMsg("*** Deleted Succesfully ***");
+                    }
+                    else
+                    {
+                        printMsg("*** Operation canceled ***");
+                    }
+                }
+            }
+        }
+
+        else if (choice == "update" || choice == "4")
+        {
+            cout << "Enter Company name for UPDATE : ";
+            cin >> temp;
+            for (int i = 0; i < size; i++)
+            {
+                if (vCards[i].imatch(temp))
+                {
+                    system("cls");
+                    printLine();
+                    cout << "\n*** OLD Details ***";
+                    vCards[i].printDetails();
+                    printLine();
+                    cout << "\n***Enter New Details ***\n";
                     vCards[i].getData();
                     system("cls");
-                    cout << "New Details :-";
+                    printLine();
+                    cout << "\n*** New Details ***";
                     vCards[i].printDetails();
+                    printLine();
                 }
             }
         }
@@ -114,14 +172,18 @@ main()
         {
             cout << "Enter Company name to search : ";
             cin >> temp;
+            flag = '0';
             for (int i = 0; i < size; i++)
             {
                 if (vCards[i].imatch(temp))
                 {
                     system("cls");
                     vCards[i].printDetails();
+                    flag = '1';
                 }
             }
+            if (flag != '1')
+                printMsg("*** Not Found ***");
         }
     }
 }
