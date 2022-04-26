bad2151264c43240596ed5db80f8fadf6baf0639
jay <jaysparmar303@gmail.com>
2022-04-26 20:12:14 +0530

    🥴 Some changes for more understanding

--- TypeConvertion/1-PrimitiveToPrimitive.cpp/2-Explicit.cpp
+++ TypeConvertion/1-PrimitiveToPrimitive.cpp/2-Explicit.cpp
@@ -6,6 +6,6 @@ main()
     cout << "Size of a                 = " << sizeof(a) << endl; // Size of a = 2
 
     // Explicit converstion
-    cout << "Size of (int)(a+0.1)      = " << sizeof((int)(a + 0.1)) << endl; // here it will convert to int explicitly
+    cout << "Size of (int) a           = " << sizeof((int) a) << endl; // here it will convert to int explicitly
     cout << "Size of (double)(a + 0.1) = " << sizeof((float)(a + 0.1)) << endl; // here it will convert to float explicitly
 }
